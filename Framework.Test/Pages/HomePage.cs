4c35ec1bacc58057f82a1bda791d22526ae2da19
abhijeetjain99 <abhijeetjain99@gmail.com>
2021-07-14 00:47:31 +0530

    Initial browser launch test

---
+++ Framework.Test/Pages/HomePage.cs
@@ -0,0 +1,27 @@
+﻿namespace Framework.Test.Pages
+{
+    using Framework.Selenium;
+    using OpenQA.Selenium;
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <seealso cref="Framework.Selenium.Base" />
+    public class HomePage : Base
+    {
+        private const string HomePageUrl = @"https://www.accuweather.com/";
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomePage" /> class.
+        /// </summary>
+        /// <param name="driver">Web driver.</param>
+        public HomePage(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+
+        public bool verifyHomePage => this.Driver.Url.Equals(HomePageUrl);
+    }
+}
--- Framework.Test/Test.cs
+++ Framework.Test/Test.cs
@@ -1,13 +1,27 @@
 ﻿namespace Framework.Test
 {
+    using Framework.Selenium;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium;
 
     [TestClass]
     public class Test
     {
+        IWebDriver Driver { get; set; }
+
+        TestContext  TestContext { get; set; }
+
+        [TestInitialize]
+        public void Init()
+        {
+            this.Driver = new WebDriverFactory().Create(BrowserType.Chrome);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+
+            this.Driver.Navigate().GoToUrl("https://www.accuweather.com/");
         }
     }
 }
