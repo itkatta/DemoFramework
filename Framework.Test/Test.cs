9e4539d0a9e83173536545ad65d528253c2d1615
abhijeetjain99 <abhijeetjain99@gmail.com>
2021-07-13 22:54:41 +0530

    Selenium package added

---
+++ Framework.Test/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace Framework.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
